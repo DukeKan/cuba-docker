6c2d3986ff7bc63d00e35c75db94c32de7d98376
d.kuznetsov <d.kuznetsov@haulmont.com>
2019-08-14 22:47:21 +0400

    Running containers for Verified issues are now colored red

--- modules/web/src/com/github/dukekan/cubadocker/web/container/ContainerBrowse.java
+++ modules/web/src/com/github/dukekan/cubadocker/web/container/ContainerBrowse.java
@@ -1,12 +1,17 @@
 package com.github.dukekan.cubadocker.web.container;
 
+import com.github.dukekan.cubadocker.entity.Container;
+import com.github.dukekan.cubadocker.service.YouTrackService;
 import com.github.dukekan.cubadocker.web.container.actions.StartContainerAction;
 import com.github.dukekan.cubadocker.web.container.actions.StopContainerAction;
+import com.github.dukekan.cubadocker.web.datasources.ContainerCollectionDatasource;
 import com.haulmont.cuba.gui.components.AbstractLookup;
 import com.haulmont.cuba.gui.components.PopupButton;
 import com.haulmont.cuba.gui.components.Table;
 
 import javax.inject.Inject;
+import java.io.IOException;
+import java.util.List;
 
 public class ContainerBrowse extends AbstractLookup {
 
@@ -14,14 +19,46 @@ public class ContainerBrowse extends AbstractLookup {
     protected Table<com.github.dukekan.cubadocker.entity.Container> containersTable;
     @Inject
     protected PopupButton containerActionsBtn;
+    @Inject
+    protected YouTrackService youTrackService;
+    @Inject
+    private ContainerCollectionDatasource containersDs;
+
 
     @Override
     public void ready() {
         initActions();
+        initTable();
     }
 
-    protected void initActions() {
+    private void initTable() {
+        List<String> verifiedIssueNumbers;
+        try {
+            verifiedIssueNumbers = youTrackService.getVerifiedIssueNumbers();
+        } catch (IOException e) {
+            showNotification("youtrackConnnectionError");
+            return;
+        }
+        containersTable.setStyleProvider((container, property) -> {
+            if (property == null && container.getState().equals("running")) {
+                String name = container.getNamesString();
+                int from = name.indexOf("knd-");
+                int to = name.indexOf("_knd_1");
+                try {
+                    String issueNumber = name.substring(from+4, to);
+                    if (verifiedIssueNumbers.contains(issueNumber)) {
+                        return "red-row";
+                    }
+                } catch (Exception e) {
+                    //do nothing
+                }
+            }
+            return null;
+        });
+    }
+
+    private void initActions() {
         containerActionsBtn.addAction(new StartContainerAction(containersTable, "startContainers"));
-        containerActionsBtn.addAction(new StopContainerAction(containersTable, "stopContainers"));
+        containerActionsBtn.addAction(new StopContainerAction(containersTable, "stopContainers", () -> containersDs.refresh()));
     }
 }
--- modules/web/src/com/github/dukekan/cubadocker/web/container/actions/StopContainerAction.java
+++ modules/web/src/com/github/dukekan/cubadocker/web/container/actions/StopContainerAction.java
@@ -15,10 +15,12 @@ import java.util.Set;
  */
 public class StopContainerAction extends ItemTrackingAction {
 
+    private final Runnable afterPerformHandler;
     protected ContainerService containerService = AppBeans.get(ContainerService.NAME);
 
-    public StopContainerAction(ListComponent target, String id) {
+    public StopContainerAction(ListComponent target, String id, Runnable afterPerformHandler) {
         super(target, id);
+        this.afterPerformHandler = afterPerformHandler;
     }
 
     @Override
@@ -27,6 +29,7 @@ public class StopContainerAction extends ItemTrackingAction {
         containerService.stopContainers(selectedContainers);
         target.getDatasource().refresh();
         target.setSelected(selectedContainers);
+        afterPerformHandler.run();
     }
 
     @Override
